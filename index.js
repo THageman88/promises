9c10c7b4c64356b0b331dfa1516d29d17dff4323
root <root@LAPTOP-EEV6NRCI>
2023-01-13 14:30:19 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let luckyNum = Math.floor(Math.random()*101);
+let luckyNum2 = Math.floor(Math.random()*101);
+let luckyNum3 = Math.floor(Math.random()*101);
+let url = "http://numbersapi.com";
+
+// 1.
+$.getJSON(`${url}/${luckyNum}?json`).then(data => {
+  console.log(data);
+});
+
+// 2.
+let luckyNums = [`${luckyNum},${luckyNum2},${luckyNum3}`];
+
+$.getJSON(`${url}/${luckyNums}?json`).then(data => {
+  console.log(data);
+});
+
+// 3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${url}/${luckyNum}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
