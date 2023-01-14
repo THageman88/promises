fd2e19ea10a2d9bc2e3ba7fd54e54308553a0e46
root <root@LAPTOP-EEV6NRCI>
2023-01-14 15:08:28 -0600

    cards

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let luckyNum2 = Math.floor(Math.random()*101);
 let luckyNum3 = Math.floor(Math.random()*101);
 let url = "http://numbersapi.com";
 
-// 1.
+// 1.call numbs api
 $.getJSON(`${url}/${luckyNum}?json`).then(data => {
   console.log(data);
 });
 
-// 2.
+// 2.make multiple requests
 let luckyNums = [`${luckyNum},${luckyNum2},${luckyNum3}`];
 
 $.getJSON(`${url}/${luckyNums}?json`).then(data => {
